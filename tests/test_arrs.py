162247053a5a8543a4f3127c68521b3801cc7815
irinaloskutova <l.irinka@mail.ru>
2023-02-12 15:05:04 +0500

    исправлен тест test_slice

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,5 +9,10 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([], 0, "test"), "test")
 
     def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+        assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+        assert arrs.my_slice([1, 2, 3, 4], 5) == []
+        assert arrs.my_slice([]) == []
+        assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
+        assert arrs.my_slice([1, 2, 3, 4], end=0) == []
